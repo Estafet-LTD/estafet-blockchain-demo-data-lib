2224e4cbb091e0e870d134b0f8fc1c455714a9df
unknown <iryna.poplavska@estafet.com>
2020-01-15 12:13:44 +0200

    https://trello.com/c/jPIrXvJS

--- src/main/java/com/estafet/blockchain/demo/data/lib/wallet/Wallet.java
+++ src/main/java/com/estafet/blockchain/demo/data/lib/wallet/Wallet.java
@@ -1,6 +1,10 @@
 package com.estafet.blockchain.demo.data.lib.wallet;
 
+import com.estafet.demo.commons.lib.properties.PropertyUtils;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,10 +37,21 @@ public class Wallet {
     }
 
     public static Wallet getWallet(String walletAddress) {
-        return new Wallet();
+        return new RestTemplate().getForObject(PropertyUtils.instance().getProperty("WALLET_MS_SERVICE_URI") + "/wallet/{walletAddress}",
+                Wallet.class, walletAddress);
     }
 
     public static List<Wallet> getWallets() {
-        return new ArrayList<>();
+        List objects = new RestTemplate().getForObject(PropertyUtils.instance().getProperty("WALLET_MS_SERVICE_URI") + "/wallets",
+                List.class);
+        List<Wallet> wallets = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        for(Object object:objects){
+            Wallet wallet = mapper.convertValue(object, new TypeReference<Wallet>(){
+
+            });
+            wallets.add(wallet);
+        }
+        return wallets;
     }
 }
