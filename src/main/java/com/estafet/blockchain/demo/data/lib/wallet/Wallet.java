eed40fbf1e22656e5401d7ff2097fd42e482d887
mike2906 <michael.ruse@estafet.com>
2020-01-17 19:37:10 +0200

    https://trello.com/c/jPIrXvJS
    Added createCreditedWallet adapter

--- src/main/java/com/estafet/blockchain/demo/data/lib/wallet/Wallet.java
+++ src/main/java/com/estafet/blockchain/demo/data/lib/wallet/Wallet.java
@@ -2,6 +2,7 @@ package com.estafet.blockchain.demo.data.lib.wallet;
 
 import com.estafet.blockchain.demo.data.lib.bank.Account;
 import com.estafet.blockchain.demo.data.lib.bank.Money;
+import com.estafet.blockchain.demo.data.lib.gateway.WalletTransfer;
 import com.estafet.demo.commons.lib.properties.PropertyUtils;
 import com.estafet.demo.commons.lib.wait.WaitUntil;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -9,6 +10,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.client.RestTemplate;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -79,9 +81,18 @@ public class Wallet {
         return wallets;
     }
     
-    public static Wallet createCreditedWallet(String accountName, String bankCurrency, Double bankBalance  ) {
+    public static Wallet createCreditedWallet(String accountName, BigInteger walletBalance  ) {
+    	Wallet wallet = Wallet.createCreditedWallet(accountName, "USD", 1000000.0, walletBalance, true);
+    	return wallet;
+    }
+    
+    public static Wallet createCreditedWallet(String accountName, String bankCurrency, Double bankBalance, BigInteger walletBalance, boolean waitForCleared  ) {
     	Account account =  Account.createCreditedAccount(accountName, bankCurrency, bankBalance );
-    	return Wallet.getWallet(account.getWalletAddress());
+    	Wallet wallet = Wallet.banktoWalletTransfer(account.getWalletAddress(), walletBalance, true);
+    	if (waitForCleared) {
+    		wallet.walletClearedWait(account.getWalletAddress());
+    	}
+    	return wallet;
     }
     
     public static Wallet createWallet(String accountName, String bankCurrency, Double bankBalance  ) {
@@ -89,11 +100,11 @@ public class Wallet {
     	return Wallet.getWallet(account.getWalletAddress());
     }
     
-    public static Wallet banktoWalletTransfer(String walletAdrress, Double amount, boolean waitForClearance  ) {
-    	Wallet wallet =  new RestTemplate().postForObject(PropertyUtils.instance().getProperty("WALLET_MS_SERVICE_URI") + "/wallet/"
-    			+ walletAdrress + "/currency-transfer/" + amount, new Wallet().setWalletAddress(walletAdrress), Wallet.class);
-		if (waitForClearance) {
-			wallet.walletClearedWait(wallet.getWalletAddress());
+    public static Wallet banktoWalletTransfer(String walletAdrress, BigInteger amount, boolean waitForBalance  ) {
+    	WalletTransfer.transferEstacoinFromBank(amount, walletAdrress );
+    	Wallet wallet = Wallet.getWallet(walletAdrress);
+		if (waitForBalance) {
+			wallet.waitForBalance(wallet.getWalletAddress(),amount);
 		}
 		return wallet;    	
     }
@@ -102,6 +113,14 @@ public class Wallet {
 		new WaitUntil() {
 			public boolean success() {
 				return getWallet(walletAdrress).getStatus().equals("CLEARED");
+			}
+		}.start();
+	}
+	
+	public void waitForBalance(String walletAdrress, BigInteger balance ) {
+		new WaitUntil() {
+			public boolean success() {
+				return BigInteger.valueOf(getWallet(walletAdrress).getBalance()).equals(balance);
 			}
 		}.start();
 	}
